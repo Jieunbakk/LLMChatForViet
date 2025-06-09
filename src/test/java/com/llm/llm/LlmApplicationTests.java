0c92cc3129e7d663d3536eb5f18aed3ed08402f3
Jieunbakk <jieun134@knu.ac.kr>
2025-06-09 20:42:39 +0900

    upload file

---
+++ src/test/java/com/llm/llm/LlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
